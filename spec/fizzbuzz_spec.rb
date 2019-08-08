943952d23cd5c6ceb963952fecd0c0d5c737716c
Gabriel Gonzalez <gabokappa@gmail.com>
2019-08-08 12:51:30 +0100

    Final working ver as a Integer method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,26 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
 
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(5.fizzbuzz).to eq 'buzz'
   end
 
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+
+  it 'returns number if not fizzbuzz' do
+    expect(2.fizzbuzz).to eq 2
+  end
+
+  it 'returns "fizz" when passed 6' do
+    expect(6.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(30.fizzbuzz).to eq 'fizzbuzz'
+  end
 end
